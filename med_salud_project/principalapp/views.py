1a0badcb5e3452b3050a677c0a13477776a9a118
Daniel Almería <daniel.alm92@gmail.com>
2024-04-10 20:40:30 -0400

    primer commit

---
+++ med_salud_project/principalapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hola(request):
+    return HttpResponse("<h1>Hola Mundo MedSalud</h1>")
+
+def principal(request):
+    return render(request=request, template_name= "pages/opciones.html", context={})
